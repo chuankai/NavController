53a28c3953d2bfbf8b6a8bbd6dc44a327999726d
Chuankai Lin <chuankai@kilab.tw>
2012-08-26 22:22:31 +0800

    Initial Commit

---
+++ Nav-3/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Nav-3
+//
+//  Created by Lin Chuankai on 8/26/12.
+//  Copyright (c) 2012 KILAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Nav-3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Nav-3
+//
+//  Created by Lin Chuankai on 8/26/12.
+//  Copyright (c) 2012 KILAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
